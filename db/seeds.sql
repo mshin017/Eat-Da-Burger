ceffc822af026b6c92b712ebaf578b063c31601c
Michelle Shin <mshin017@ucr.edu>
2018-06-19 21:59:54 -0700

    initial commit with all the folders

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Double", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BLT Extreme", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jalepeño Exploder", false);
+
